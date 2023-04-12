142a97b7ec1ec6aac2e381bbd3d57b7bc5924a24
Pratham Manjhi <prathammanjhi2@gmail.com>
2023-04-12 10:32:05 +0530

    all commit

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum{
     public static void main(String args[]){
         int a = 3;
         int b =6;
-        System.out.println(a+b);
+        System.out.println("Hello"+ (a+b));
     }
     
 }
